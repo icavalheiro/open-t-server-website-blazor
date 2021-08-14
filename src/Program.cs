8da7fd929f03d7b583ab2747127ed599ccc3e006
Ivan <ivanscavalheiro@gmail.com>
2021-08-14 12:12:50 -0300

    removed the static generation since this test was not worth it

--- src/Program.cs
+++ src/Program.cs
@@ -18,46 +18,9 @@ namespace TibiaWebsite
         public static void Main(string[] args)
         {
             var hostBuilder = CreateHostBuilder(args);
-            if (args.Length > 0 && args[0] == "generate")
-            {
-                Task.WaitAll(GenerateStatic(hostBuilder));
-            }
-            else
-                hostBuilder.Build().Run();
+            hostBuilder.Build().Run();
         }
 
-        private static async Task GenerateStatic(IHostBuilder hostBuilder, string distPath = "./dist", int port = 8744)
-        {
-            Console.WriteLine("Server Side Generation with \"wget\". Please make sure \"wget\" is installed and in path");
-            hostBuilder.ConfigureWebHost(builder =>
-            {
-                builder.UseUrls($"http://0.0.0.0:{port}");
-
-            });
-
-            var host = hostBuilder.Build();
-            var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
-
-            lifetime.ApplicationStarted.Register(async () =>
-            {
-                Console.WriteLine("Deleting dist folder...");
-                if (Directory.Exists(distPath))
-                {
-                    Directory.Delete(distPath, true);
-                }
-
-                Console.WriteLine($"Starting wget, saving files to {distPath}");
-                var process = Process.Start("wget", $"-m http://localhost:{port} -P {distPath} -nv -E -nH");
-
-                await process.WaitForExitAsync();
-
-                Console.WriteLine("Generation completed, stoppping server...");
-                await host.StopAsync();
-            });
-
-            Console.WriteLine($"Starting server for generation at port {port}");
-            await host.RunAsync();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
